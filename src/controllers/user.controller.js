d96c5eb1f73b6bfd300e7e5570c9ee41895f7f15
Isha Dewangan <dewanganisha05@gmail.com>
2024-06-15 23:55:42 -0700

    fix in logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -185,8 +185,11 @@ const logoutUser = asyncHandler( async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            // $set: {
+            //     refreshToken: undefined
+            // }
+            $unset: {
+                refreshToken: 1 // this removes the field from document
             }
         },
         {
