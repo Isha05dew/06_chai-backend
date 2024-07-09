15f22fdbb62dcdbeae244d91505d80d849e4288b
Isha Dewangan <dewanganisha05@gmail.com>
2024-07-09 04:04:25 -0700

    Add healthcheck controllers and routes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import likeRouter from './routes/like.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
+import healthcheckRouter from './routes/healthcheck.routes.js'
 
 
 
@@ -36,6 +37,7 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 // http://localhost:8000/api/v1/users/register
 
